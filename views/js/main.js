f36ce6df49fe7b70e03c344fca84b2dd14dc222e
Gravedd <troperread@gmail.com>
2021-11-20 20:19:14 +0400

    import makeup

---
+++ views/js/main.js
@@ -0,0 +1,11 @@
+/*
+document.addEventListener("DOMContentLoaded", function (){
+    document.body.style.opacity = 1;
+});
+let links = document.querySelectorAll('a');
+links.forEach(function (link) {
+    link.addEventListener('click', function () {
+        document.body.style.opacity = 0;
+    });
+});
+*/

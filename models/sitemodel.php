33132ccead7dd011804b5acc3d74e6da5e0ac300
Gravedd <troperread@gmail.com>
2021-11-21 14:49:39 +0400

    Database posts, temporary pictures
    output of posts, added temporary pictures

--- models/sitemodel.php
+++ models/sitemodel.php
@@ -11,7 +11,8 @@ class Site {
         global $db, $connection;//глобальные переменные
         $query = "SELECT * FROM articles where `id`!=1 AND `id`!=2 AND `id`!=3";//sql запрос. Здесь номера статей которые не нужно искать "AND `id`!=3"
         $result = mysqli_query($connection, $query) or die(mysqli_error($connection));//получаем данные
-        $data = mysqli_fetch_assoc($result);//конверируем полученные данные в массив
+        for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);//конверируем полученные данные в массив
+
         return array($data);//возращаем массив
     }
 }
--- views/includes/indexcontent.php
+++ views/includes/indexcontent.php
@@ -3,7 +3,7 @@
     <div class="about">
         <!--БЛОК ОБО МНЕ-->
         <div class="leftside" tabindex="0">
-            <div class="imgwrapper"></div>
+            <div class="imgwrapper" style="background-image: url('../../uploads/images/<?php echo $aboutme[0]['image']; ?>');"></div>
             <h2>
                 <a href="/articles/<?php echo $aboutme[0]['id']; ?>">
                     <?php echo $aboutme[0]['title']; ?></h2>
@@ -18,7 +18,7 @@
 
             <!--БЛОК SOFT Skills-->
             <div class="aboutart">
-                <div class="imgwrapper"></div>
+                <div class="imgwrapper" style="background-image: url('../../uploads/images/<?php echo $softskills[0]['image']; ?>');"></div>
                 <h2>
                     <a href="/articles/<?php echo $softskills[0]['id']; ?>">
                     <?php echo $softskills[0]['title']; ?></h2>
@@ -28,7 +28,7 @@
 
             <!--БЛОК HARD SKILLS-->
             <div class="aboutart">
-                <div class="imgwrapper"></div>
+                <div class="imgwrapper" style="background-image: url('../../uploads/images/<?php echo $hardskills[0]['image']; ?>');"></div>
                 <h2>
                     <a href="/articles/<?php echo $hardskills[0]['id']; ?>">
                     <?php echo $hardskills[0]['title']; ?></h2>
@@ -40,25 +40,15 @@
 
     <div class="h2-like">Мои проекты</div>
     <div class="other">
-        <div class="otherart">
-            <div class="imgwrapper"></div>
-            <a href="/articles/1"><h2>What is Lorem Ipsum?</h2>
-            <span class="dateart">2021-11-14</span></a>
-        </div>
-        <div class="otherart">
-            <div class="imgwrapper"></div>
-            <a href="/articles/1"><h2>What is Lorem Ipsum?</h2>
-            <span class="dateart">2021-11-14</span></a>
-        </div>
-        <div class="otherart">
-            <div class="imgwrapper"></div>
-            <a href="/articles/1"><h2>What is Lorem Ipsum?</h2>
-            <span class="dateart">2021-11-14</span></a>
-        </div>
-        <div class="otherart">
-            <div class="imgwrapper"></div>
-            <a href="/articles/1"><h2>What is Lorem Ipsum?</h2>
-            <span class="dateart">2021-11-14</span></a>
-        </div>
+        <?php //вывод статьи.
+        for ($i=0; $i < count($articles[0]); $i++) {//пока счетчик цикла меньше кол-ва статей полученных, то выводить статью?>
+            <div class="otherart">
+                <div class="imgwrapper" style="background-image: url('../../uploads/images/<?php echo $articles[0][$i]['image']; ?>');"></div>
+                <a href="/articles/<?php echo $articles[0][$i]['id']; ?>"><h2><?php echo $articles[0][$i]['title']; ?></h2>
+                    <span class="dateart"><?php echo $articles[0][$i]['pubdate']; ?></span></a>
+            </div>
+        <?php
+        }
+        ?>
     </div>
 </main>

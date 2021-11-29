723772810ab910730af8044b4100b6bfb5e4dcaf
Gravedd <troperread@gmail.com>
2021-11-29 22:15:55 +0400

    change link
    изменил ссылку

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -2,6 +2,6 @@
     <div><a target="_blank" href="mailto:troperread@gmail.com">troperread@gmail.com</div></a>
     <div><a target="_blank" href="https://vk.com/graveddd">Vk</div></a>
     <div><a target="_blank" href="https://www.youtube.com/">Youtube</div></a>
-    <div class="opensourse"><a target="_blank" href="https://github.com/Gravedd">This website is open source</div></a>
+    <div class="opensourse"><a target="_blank" href="https://github.com/Gravedd/Portfolio">This website is open source</div></a>
 </footer>
 <script src="views/js/main.js"></script>

9e90dac738f3761bf417edb6b600c38cae2d96ad
Gravedd <troperread@gmail.com>
2021-12-01 21:56:40 +0400

    test

--- views/index.php
+++ views/index.php
@@ -1,4 +1,15 @@
-<!--главная страница-->
+<!DOCTYPE HTML>
+<HTML>
+<head>
+    <title>Юрий Сенин — Веб-разработчик</title>
+    <link href="/views/css/style.css" rel="stylesheet" type="text/css">
+    <link href="/views/css/adaptive.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">-->
+    <meta name="description" content="Юрий Сенин — сайт веб-разработчика. HTML, CSS, JS, PHP...">
+</head>
+<body>
 <div class="container">
 <?php
 include_once('includes/header.php');//шапка
@@ -6,3 +17,4 @@ include_once('includes/indexcontent.php');//файл контента
 include_once('includes/footer.php');//файл контента
 ?>
 </div>
+</body>
